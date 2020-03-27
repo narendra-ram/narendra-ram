c00dde2a2112d5345ffad562768df4648d44882a
Narendra Krishna Ram (via Travis CI) <nkr@aroliant.com>
2020-03-27 22:39:15 +0000

    Deploy narendra-ram/narendra-ram to github.com/narendra-ram/narendra-ram.git:gh-pages

---
+++ pages/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <div>
+    <div class="Header">
+      <nav class="container navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#">Narendra Ram</a>
+        <button
+          class="navbar-toggler d-lg-none"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hire Me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h1>Hi, I'm Narendra Ram</h1>
+            <h3>Full Stack Designer</h3>
+            <div class="SocialLinks">
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
+              <div class="SocialLink"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <hr />
+    </div>
+    <section id="portfolio">
+      <div class="container">
+        <h3>Portfolio</h3>
+        <h4 class="FontRegular">Here are some of my works</h4>
+        <div class="row mt-3">
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">GTrendz Online</h3>
+              <h5>A Multivendor E-Commerce</h5>
+              <p
+                class="PortfolioItem_description"
+              >I worked on the frontend requirements for the website, react-native application, and the angular dashboards. Some of my tasks are such as creating pages, multiple layouts, responsiveness, etc.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Blaz App</h3>
+              <h5>Deployments & Scaling</h5>
+              <p
+                class="PortfolioItem_description"
+              >I have been working on building the frontend part for the blaz app and the landing pages for the website. It’s a work in progress.</p>
+              <a href="https://blaz.app/" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Instant Delivery App</h3>
+              <h5>Delivery Platform</h5>
+              <p
+                class="PortfolioItem_description"
+              >This app designed from scratch using UX Research techniques such as interviews, surveys, from paper sketches to a digital prototype.</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Nuxt.js Websites</h3>
+              <h5>Small Businesses & Companies</h5>
+              <p
+                class="PortfolioItem_description"
+              >It’s has been my choice for building websites for small scales businesses and corporate websites. Here are some of the nuxt.JS projects that i recently built.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">VS App</h3>
+              <h5>Live Streaming Platform</h5>
+              <p
+                class="PortfolioItem_description"
+              >It’s has been my choice for building websites for small scales businesses and corporate websites. Here are some of the nuxt.JS projects that i recently built.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="PortfolioItem">
+              <h3 class="FontLight">Coaching Cards</h3>
+              <h5>Mobile App</h5>
+              <p
+                class="PortfolioItem_description"
+              >It’s has been my choice for building websites for small scales businesses and corporate websites. Here are some of the nuxt.JS projects that i recently built.</p>
+              <a href="https://gtrendz.online" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+        </div>
+        <h4 class="FontRegular mt-5">Open Source Contributions</h4>
+        <div class="row mt-1">
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="OpenSource_item">
+              <h4>DevTools</h4>
+              <p
+                class="PortfolioItem_description"
+              >It’s a small project, a website for developer tools. I contributed to redesigning the site with the latest bootstrap framework a while ago.</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 p-3 p-sm-3 p-md-3 p-lg-3">
+            <div class="OpenSource_item">
+              <h4>Felix App</h4>
+              <p
+                class="PortfolioItem_description"
+              >I have been contributing to design and frontend development (UX/UI) for this dockerized file manager on GitHub</p>
+              <a href="#" rel="no-follow" class="ProjectLink">website</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <hr />
+    </div>
+    <section>
+      <div class="container">
+        <h3>Case Study</h3>
+        <h4 class="FontRegular">Explore the case studies of my projects</h4>
+
+        <div id="CaseStudy" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#CaseStudy" data-slide-to="0" class="active"></li>
+            <li data-target="#CaseStudy" data-slide-to="1"></li>
+            <li data-target="#CaseStudy" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img data-src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Title</h3>
+                <p>Description</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img data-src="holder.js/900x500/auto/#666:#444/text:Second slide" alt="Second slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Title</h3>
+                <p>Description</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img data-src="holder.js/900x500/auto/#666:#444/text:Third slide" alt="Third slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Title</h3>
+                <p>Description</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#CaseStudy" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#CaseStudy" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <hr />
+    </div>
+    <section>
+      <div class="container">
+        <h3>My Story &amp; Experience</h3>
+        <h4 class="FontRegular">About Me</h4>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
